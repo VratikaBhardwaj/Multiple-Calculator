a472df763d25a17fafe9bba28683f656c03a0a36
VratikaBhardwaj <111068952+VratikaBhardwaj@users.noreply.github.com>
2023-07-31 21:06:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+// THis is percent calculator script
+function mypercent() {
+  debugger;
+  var per = document.getElementById("percent").value;
+  var num = document.getElementById("number").value;
+  var res = (num / 100) * per;
+  document.getElementById("result").value = res;
+}
 
+// this is BMI Script
+function mybmi() {
+  var kg = document.getElementById("weight").value;
+  var mt = document.getElementById("height").value;
+  document.getElementById("bmiresult").value = kg / (mt * mt);
+
+  {
+    function myhealth() {
+      if (bmiresult >= 18.5 || bmiresult <= 24.9) {
+        
+          (document.getElementById(healthy).innerHTML = "you are over-weight")
+        ;
+      } else if (bmiresult >= 25.0 || bmiresult <= 29.9) {
+        
+          (document.getElementById(healthy).innerHTML = "you are over-weight")
+        ;
+      } else {
+        
+          (document.getElementById(healthy).innerHTML = "you are under-weight")
+        ;
+      }
+    }
+}
+}
